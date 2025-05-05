4300973b3ff32e597c1bcacad7e859d3bebb0f88
Dryzera <endryusdaniel6321@gmail.com>
2025-05-05 19:30:12 -0300

    create new pages and developing models

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './styles/global.css';
 import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
     return (
