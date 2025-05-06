0bb6f996515a34d403e717f6fa29231f94cac516
Dryzera <endryusdaniel6321@gmail.com>
2025-05-06 18:09:05 -0300

    implement on code a useRef

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,27 @@ import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { Cycles } from '../Cycles';
 import { PlayCircleIcon } from 'lucide-react';
+import { useRef } from 'react';
 
 export function MainForm() {
+    // const [taskName, setTaskName] = useState('');
+    const taskNameInput = useRef<HTMLInputElement>(null);
+    
+    const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+    
     return (
-        <form action='' className='form'>
+        <form onSubmit={handleSubmitForm} action='' className='form'>
         <div className='formRow'>
             <DefaultInput
                 labelText='tarefa'
                 type='text'
                 id='input'
                 placeholder='ex.: estudar matemática'
+                // value={taskName}
+                // onChange={e => setTaskName(e.target.value)}
+                ref={taskNameInput}
             />
         </div>
         <div className='formRow'>
